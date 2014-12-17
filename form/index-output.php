23550405577c49989e2e6c62029e0dd04e9ca07a
Yuka Sasaki <t13408ys@gmail.com>
2014-12-18 02:32:34 +0900

    updated index-output.php

--- form/index-output.php
+++ form/index-output.php
@@ -1,26 +1,27 @@
 <?php
-
-$link = sqlite_open('test.db', 0666, $sqliteerror);
-if (!$link) {
-    die('接続失敗です。'.$sqliteerror);
-}
-
-print('接続に成功しました。<br>');
-
-$sql = "SELECT id, name FROM datas";
-$result = sqlite_query($link, $sql, SQLITE_BOTH, $sqliteerror);
-if (!$result) {
-    die('クエリーが失敗しました。'.$sqliteerror);
-}
-
-for ($i = 0 ; $i < sqlite_num_rows($result) ; $i++){
-    $rows = sqlite_fetch_array($result, SQLITE_ASSOC);
-    print('id='.$rows['id']);
-    print(',name='.$rows['name'].'<br>');
-}
-
-sqlite_close($link);
-
-print('切断しました。<br>');
-
-?>
+    
+    ////データベースの名前はtest.sqlite3
+    ////テーブルの名前はtest
+    ////カルムはname picture title info
+    
+    
+    
+    // 接続（DBが存在しない場合はDB作成）
+    $db = new SQLite3('test.sqlite3');
+    if (!$db) {
+        die('接続失敗です'.$sqliteerror);
+    }
+    print('接続に成功しました。<br>');
+    
+    
+    $result = $db->query('SELECT * FROM test');
+    while ($row = $result->fetchArray()) {
+        echo $row[0],"\n";
+        echo $row[1],"\n";
+        echo $row[2],"\n";
+        echo $row[3],"\n";
+    }
+    
+    $db->close();
+    
+    ?>
